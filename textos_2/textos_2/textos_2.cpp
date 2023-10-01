7d3ab4b8206a351f3bd285cd98e4eb4b27c85751
Walter Meléndez <100178332+waltermelendez@users.noreply.github.com>
2023-10-01 15:53:07 -0600

    INTENTO DE CAMBIOS
    INTENTO DE CAMBIOS EN LA VERSION 1.1

--- textos_2/textos_2/textos_2.cpp
+++ textos_2/textos_2/textos_2.cpp
@@ -4,6 +4,11 @@
 #include <fstream>
 #include <stdlib.h>
 #include<string>
+#include<filesystem>
+#include <vector>
+
+ // 
+
 
 
 using namespace std;
@@ -31,6 +36,7 @@ int main()
 		}
 	}*/
 	lectura();
+	
 
 
 
@@ -58,52 +64,69 @@ int main()
 //}
 void lectura() {
 	ifstream archivo;
-	string texto;
 	string s;
+	int x = 1;
+	cout << "link del porgrama";
+	cin >> x;
+	for (int i = 0;i < x;i++)
+	{
+
+	}
+	string texto;
 	int linea = 0;
 	//string nombre_artista, n_cancion, duracion;
-	
-	cout << "diga en nombre de ubicacion. Nota por cada debe de agregar '\\\.' debe poner un'/' y al final poner '.txt' ";
+	cout << "------------------------\n";
+	cout << "diga en nombre de ubicacion. Nota por cada debe de agregar '\\\' debe poner un'/' y al final poner '.txt' " << endl;
 	//es con el nombre del archivo
 	getline(cin, s);
+	
+		
+		while (!archivo.eof())
+		{
+			archivo.open(s.c_str(), ios::in);//abrir arcito en modo lectura
+			if (archivo.fail()) {
+				cout << "El archivo no esiste";
+				exit(1);
+			}
+			else {
+				while (!archivo.eof()) {//mientras QUE NO sea el final del arhico
+					getline(archivo, texto);
+					linea++;// conatdor lineas del texto.
+					/*cout << texto << endl;*/
+					//string g = "Hells Bells||AC/DC||5:12";
+					int encontrar = texto.find("||");
+					int pimero = 0;
 
-	archivo.open(s.c_str(), ios::in);//abrir arcito en modo lectura
-	if (archivo.fail()) {
-		cout << "El archivo no esiste";
-		exit(1);
-	}
-	while (!archivo.eof()) {//mientras QUE NO sea el final del arhico
-		getline(archivo, texto);
-		linea++;// conatdor lineas del texto.
-		/*cout << texto << endl;*/
-		//string g = "Hells Bells||AC/DC||5:12";
-		int encontrar = texto.find("||");
-		int pimero = 0;
 
+					while (encontrar != string::npos)//mientras que encontrar no sea igual a encontar al caracter divisor seguira buscando el dato hasta la segundaa parte
+					{/*por que la segunda parte porque, pirmero necesitamos convertir los numeros para poder usarlos, segundo su limite es cuando sea igual
+					 npos indica la posicion maxima que se puede guarda*/
+					 //El elemento fue encontrado, la posición se almacena en find
+						string grupo = texto.substr(pimero, encontrar - pimero);//pimero, donde empiza; encontrar - pimero la cantiadad de caracteres
+						cout << grupo << " ";
+						pimero = encontrar + 2;
+						encontrar = texto.find("||", pimero);
 
-		while (encontrar != string::npos)//mientras que encontrar no sea igual a encontar al caracter divisor seguira buscando el dato hasta la segundaa parte
-		{/*por que la segunda parte porque, pirmero necesitamos convertir los numeros para poder usarlos, segundo su limite es cuando sea igual
-		 npos indica la posicion maxima que se puede guarda*/
-		 //El elemento fue encontrado, la posición se almacena en find
-			string grupo = texto.substr(pimero, encontrar - pimero);//pimero, donde empiza; encontrar - pimero la cantiadad de caracteres
-			cout << grupo << " ";
-			pimero = encontrar + 2;
-			encontrar = texto.find("||", pimero);
+					}
+					string numero = texto.substr(pimero);
+					cout << numero << endl;
+					int espaico = texto.find("\n");
+					int pri = 0;
+				}
+
+				cout << "sus canciones subidas son: " << linea << endl;//para saber cuantas canciones fueron subidas.
+
+				int n = s.find_last_of("/\\");
+				if (n != std::string::npos) {
+					cout << s.substr(n + 1);
+				}
+				archivo.close();
+			}
 
 		}
-		string numero = texto.substr(pimero);
-		cout << numero << endl;
-		int espaico = texto.find("\n");
-		int pri = 0;
-	}
-
-	cout << "sus canciones subidas son: " << linea << endl;//para saber cuantas canciones fueron subidas.
-
-	int n = s.find_last_of("/\\");
-	if (n != std::string::npos) {
-		cout << s.substr(n + 1);
-	}
-	archivo.close();
+	
+	
+	
 
 
 }
